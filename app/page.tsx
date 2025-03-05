8bf06d19086578972c8744765bef1d71cd86a892
runhannah <17864983+RunHannah@users.noreply.github.com>
2025-03-04 21:24:38 -0800

    wip: landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,54 @@
 "use client";
+import Image from "next/image";
+import PetCard from "@/components/PetCard";
 
 export default function Home() {
-  return <div>Hello Dogs</div>;
+  const pets = [
+    {
+      name: "Meg and Harry",
+      src: "/dog-two.jpg",
+      alt: "photo of meg and harry",
+    },
+    { name: "Simu", src: "/dog-three.jpg", alt: "photo of simu" },
+    { name: "Ramona", src: "/dog-four.jpg", alt: "photo of ramona" },
+    { name: "Rosalia", src: "/dog-five.jpg", alt: "photo of rosalia" },
+  ];
+
+  return (
+    <div className="min-h-screen">
+      <section className="relative w-full h-[500px]">
+        <Image
+          src="/dog-one.jpg"
+          fill
+          objectFit="cover"
+          alt="hero photo of a happy dog"
+          className="object-cover w-full h-full"
+        />
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
+        <div className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full px-2.5">
+          <h1 className="text-white text-5xl font-bold text-center mb-5">
+            Find your new best friend!
+          </h1>
+          <h2 className="text-white text-2xl font-bold text-center">
+            Browse dogs from our network of shelters and rescues.
+          </h2>
+        </div>
+      </section>
+      <section>
+        <div className="my-10 text-center text-2xl text-purple-900 font-bold">
+          <p>Dogs Available for Adoption Nearby</p>
+        </div>
+        <div className="flex flex-row justify-evenly mb-10">
+          {pets.map((pet) => (
+            <PetCard
+              key={pet.name}
+              name={pet.name}
+              src={pet.src}
+              alt={pet.src}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
---
+++ components/PetCard.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { Card, CardContent } from "@/components/ui/card";
+
+interface PetCardProps {
+  name: string;
+  src: string;
+  alt: string;
+}
+
+export default function PetCard({ name, src, alt }: PetCardProps) {
+  return (
+    <Card className="w-fit h-[300px] border-purple-900 border-2 py-0 m-2">
+      <Image
+        className="rounded-tl-lg rounded-tr-lg"
+        src={src}
+        alt={alt}
+        width={300}
+        height={30}
+      />
+      <CardContent>
+        <p>{name}</p>
+      </CardContent>
+    </Card>
+  );
+}
